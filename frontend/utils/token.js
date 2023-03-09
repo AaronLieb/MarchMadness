4ca28e789afc0cec016bc83b87ed120cb005058d
Justin Stitt <jstitt007@gmail.com>
2023-03-09 02:18:01 -0800

    team creation MVP

--- frontend/utils/token.js
+++ frontend/utils/token.js
@@ -1,5 +1,17 @@
-let base = "https://api.jstitt.dev/acmmm/sheet"
-
 export const getToken = async () => {
-  return (await fetch(`${base}/get_token`).then(response => response.json())).token
+  let team_name = document.getElementById('team-name-input').value;
+  let token_box = document.getElementById('token-box');
+  let submit_button = document.getElementById('submit-team');
+
+  submit_button.setAttribute("aria-busy", "true");
+  console.log('team_name: ', team_name)
+
+  fetch(`https://api.jstitt.dev/acmmm/sheet/create_team?team_name=${team_name}`, {
+    method: "POST",
+  }).then(res => res.json())
+    .then(json => {
+      token_box.style.visibility = 'visible';
+      token_box.dataset.content = json.token
+      submit_button.setAttribute("aria-busy", "false");
+    });
 }
