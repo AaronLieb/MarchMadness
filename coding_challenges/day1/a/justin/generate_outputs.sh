560cab40abd4b1c00ec84527ecd3c399ee15b518
Justin Stitt <jstitt007@gmail.com>
2023-03-12 16:41:33 -0700

    adjust script

--- coding_challenges/day1/a/justin/generate_outputs.sh
+++ coding_challenges/day1/a/justin/generate_outputs.sh
@@ -1,15 +1,7 @@
 #! /usr/bin/bash
-#
-if [ -d "outputs" ]
-then
-  echo "Clearing outputs directory"
-  rm -rf "./outputs"
-fi
-
-mkdir "./outputs"
-
 echo "Running solution... [0.in, 99.in]"
 for i in {0..99}
 do
   python solve.py < "../inputs/$i.in" >> "results.out"
 done
+echo "Donezo"
