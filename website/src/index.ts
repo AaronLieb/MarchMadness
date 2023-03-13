6cfd6879f9e828b22fabcd2057e54ad0306121d8
Justin Stitt <jstitt007@gmail.com>
2023-03-13 02:58:14 -0700

    add node-fetch dependency

--- website/src/index.ts
+++ website/src/index.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import http from 'http';
 import path from 'path';
+import fetch from 'node-fetch'
 
 dotenv.config()
 
@@ -15,7 +16,7 @@ app.use(cookieParser())
 app.get('/inputs/:day/:part', async (req, res) => {
   if (!req.cookies.team) res.status(400).send('Missing team name');
   const url = `https://api.jstitt.dev/acmmm/sheet/get_index?team_name=${req.query.team_name}`;
-  const index = (await fetch(url).then(res => res.json())).index;
+  const index = (await fetch(url).then(res => res.json()))['index']
   const file = `/../inputs/${req.params.day}/${req.params.part}/${index}.in`;
   res.sendFile(path.join(__dirname + file));
 });
