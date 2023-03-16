aab59f95fbd261560e8b8bf91806625b61c233ac
Aaron Lieberman <aarnliebrmn@gmail.com>
2023-03-16 04:44:25 -0700

    results

--- coding_challenges/day4/a/aaron/generate_outputs.sh
+++ coding_challenges/day4/a/aaron/generate_outputs.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/bash
 echo "Running solution... [0.in, 99.in]"
-for i in {0..99}
+for i in {83..99}
 do
+  if [  $((i % 10)) -eq 0  ]
+  then
+    echo $i
+  fi
   python solve.py < "../inputs/$i.in" >> "results.out"
 done
 echo "Donezo"
