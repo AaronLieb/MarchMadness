927f67a248b695921f010c7bd82722317707e1dd
Aaron Lieberman <aarnliebrmn@gmail.com>
2023-03-17 02:40:31 -0700

    input fixes

--- coding_challenges/day5/a/generate.py
+++ coding_challenges/day5/a/generate.py
@@ -10,7 +10,7 @@ MIN_CHECKS = 10000
 MAX_CHECKS = 20000
 NUM_BRACKETS = 50
 MIN_MULT = 1.1
-MAX_MULT = 1.29
+MAX_MULT = 1.34
 
 if DEBUG:
     MIN_CHECKS = 22
@@ -37,12 +37,13 @@ def percent(num):
     return str(int(num*100)) + "%"
 
 
-print(NUM_BRACKETS)
+print(NUM_BRACKETS + 1)
 print(n)
 
+print(money(0), money(brackets[0] - 1), percent(0))
 for i in range(NUM_BRACKETS):
     brackets.append(int(brackets[i] * random.uniform(MIN_MULT, MAX_MULT)))
-    print(money(brackets[i]), money(brackets[i + 1] - 1), percent((i/NUM_BRACKETS)**(7/10)))
+    print(money(brackets[i]), money(brackets[i + 1] - 1), percent(((i + 1)/NUM_BRACKETS)**(7/10)))
 
 for i in range(n):
     amount = abs(np.random.normal(0, 10000, size=(1))[0])
---
+++ coding_challenges/day5/c/generate.py
@@ -0,0 +1,8 @@
+# Generate a list of reported incomes
+
+# Generate a list of name -> account number, amount in account
+
+
+# prime factorization 
+# Generate a list of transactions   account number 20% -> account number
+
